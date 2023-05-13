bb7aa5ed397f10cb864831dcca563837e0b5e6c7
hyeonjeong Park <py0429@ewhain.net>
2023-05-13 14:58:27 +0900

    init

---
+++ src/main/java/com/server/cds3/Cds3Application.java
@@ -0,0 +1,13 @@
+package com.server.cds3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cds3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cds3Application.class, args);
+	}
+
+}
