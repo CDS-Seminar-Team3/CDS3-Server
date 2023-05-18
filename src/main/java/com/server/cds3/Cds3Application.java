5b9a97055c70710da1c5c432e723dda45c1f52d3
Hong Jun <rhkgkrwkhj@naver.com>
2023-05-19 05:36:11 +0900

    #4 [FEAT] 청원 글 작성 API 완료

--- src/main/java/com/server/cds3/Cds3Application.java
+++ src/main/java/com/server/cds3/Cds3Application.java
@@ -1,15 +1,19 @@
 package com.server.cds3;
 
+import com.server.cds3.domain.Petition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.ArrayList;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class Cds3Application {
-
+	public static ArrayList<Petition> petitionList;
 	public static void main(String[] args) {
 		SpringApplication.run(Cds3Application.class, args);
+		petitionList = new ArrayList<>();
 	}
 
 }
